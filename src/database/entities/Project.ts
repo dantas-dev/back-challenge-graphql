f92083f1f3ea195af84ce62667814bb6a827539d
Jefferson da Silva <je_jss@hotmail.com>
2021-05-27 03:19:21 -0300

    Update entities/objectType

--- src/database/entities/Project.ts
+++ src/database/entities/Project.ts
@@ -1,5 +1,5 @@
-import { Field, ID, ObjectType } from "type-graphql";
-import { Column, Entity, ManyToOne, OneToMany, PrimaryColumn } from "typeorm";
+import { Field, Float, ID, ObjectType } from "type-graphql";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
 import { v4 as uuid } from "uuid";
 
 import { IUser, User } from "./User";
@@ -25,14 +25,18 @@ class Project implements IProject {
   @Column()
   name: string;
 
-  @Field((type) => String)
+  @Field((type) => Float)
   @Column()
   price: number;
 
   @Field((type) => String)
-  @ManyToOne(() => User, (user) => user.id)
   @Column()
   user_id: string;
+
+  @Field((type) => User)
+  @ManyToOne(() => User, (user) => user.id)
+  @JoinColumn({ name: "user_id" })
+  user: User;
 
   constructor() {
     if (!this.id) {
--- src/database/entities/User.ts
+++ src/database/entities/User.ts
@@ -1,11 +1,14 @@
 import { Field, ID, ObjectType } from "type-graphql";
-import { Column, Entity, PrimaryColumn } from "typeorm";
+import { Column, Entity, JoinColumn, OneToMany, PrimaryColumn } from "typeorm";
 import { v4 as uuid } from "uuid";
+
+import { IProject, Project } from "./Project";
 
 export interface IUser {
   id?: string;
   name: string;
   email: string;
+  projects?: IProject;
 }
 
 @ObjectType()
@@ -22,6 +25,11 @@ class User implements IUser {
   @Field((type) => String)
   @Column()
   email: string;
+
+  @Field((type) => [Project], { nullable: true })
+  @JoinColumn()
+  @OneToMany(() => Project, (project) => project.id)
+  projects?: Project;
 
   constructor() {
     if (!this.id) {
