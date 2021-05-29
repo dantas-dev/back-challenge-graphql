049fa25d79156b8d12f38b6cb91fe00f7f33e95d
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2021-05-29 16:13:24 -0300

    create users migration

---
+++ src/database/migrations/1622315382445-CreateUsers.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUsers1622315382445 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "email",
+            type: "varchar",
+            isUnique: true,
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users");
+  }
+}
