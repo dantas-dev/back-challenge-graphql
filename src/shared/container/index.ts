87d6a55f04a1eb39a4a495ccbc57282db50591c5
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2021-05-30 11:51:53 -0300

    resister projects repository pg database singleton

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,16 @@
 import { container } from "tsyringe";
 
+import { ProjectRepositoryPG } from "../../modules/projects/repositories/implementations/ProjectRespositoryPG";
+import { IProjectRepository } from "../../modules/projects/repositories/IProjectRepository";
 import { UsersRepositoryPG } from "../../modules/user/repositories/implementations/UserRepositoryPG";
 import { IUsersRepository } from "../../modules/user/repositories/IUserRepository";
 
 container.registerSingleton<IUsersRepository>(
   "UsersRepositoryPG",
   UsersRepositoryPG
+);
+
+container.registerSingleton<IProjectRepository>(
+  "ProjectRepositoryPG",
+  ProjectRepositoryPG
 );
