07e31d5ad10e4adc50a0bd6b2bf38ed0aa318631
Fabio Santos <fabiosantosrj@gmail.com>
2021-06-03 18:52:43 -0300

    incluído catch na chamada do listen.

--- src/server.ts
+++ src/server.ts
@@ -9,5 +9,5 @@ import resolvers from '@resolvers/index';
 import db from '@db-models/index';
 
 const server = new ApolloServer({ typeDefs, resolvers, context: async ({req}) => ({ db, req }), playground: {endpoint: "/graphql"} });
-server.listen(process.env.SRV_PORT).then(({ url }) => console.log(`Server started at ${url}`));
+server.listen(process.env.SRV_PORT).then(({ url }) => console.log(`Server started at ${url}`)).catch((err)=> {console.log(`error started server at ${err}`)});
 
