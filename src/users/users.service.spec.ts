24a2d3c6c0540759e1bce3d9799038619513c7bf
uigor <uigor.silva@acad.ifma.edu.br>
2021-10-19 17:36:29 -0300

    test: adiciona user.service.spec

---
+++ src/users/users.service.spec.ts
@@ -0,0 +1,14 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from './users.service';
+
+describe('UsersService', () => {
+  let service: UsersService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UsersService],
+    }).compile();
+
+    service = module.get<UsersService>(UsersService);
+  });
+});
