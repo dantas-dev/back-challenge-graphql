aec103e03f59a1920857c935267812597cb338cb
uigor <uigor.silva@acad.ifma.edu.br>
2021-10-19 18:40:06 -0300

    refactor: remove tipo da model

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -4,7 +4,7 @@ import { Field, ObjectType } from '@nestjs/graphql';
   modelName: 'users',
 })
 @ObjectType()
-export class User extends Model<User> {
+export class User extends Model {
   @Field()
   id: number;
 
