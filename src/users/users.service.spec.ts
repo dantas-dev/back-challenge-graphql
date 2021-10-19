ee8383df6a04d88ce09d55b54acab575f7fa4d4a
uigor <uigor.silva@acad.ifma.edu.br>
2021-10-19 18:48:14 -0300

    test: importa UsersRepository

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { UsersRepository } from './repositories/users.repository';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
@@ -6,7 +7,13 @@ describe('UsersService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: UsersRepository,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
