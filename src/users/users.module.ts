bfddea34cfafb9661eab9e638da857f58b8d9511
uigor <uigor.silva@acad.ifma.edu.br>
2021-10-19 18:39:36 -0300

    feat: adiciona  conexao com banco de user

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
-
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './entities/user.entity';
+import { UsersRepository } from './repositories/users.repository';
 @Module({
-  imports: [],
-  providers: [UsersResolver, UsersService],
+  imports: [SequelizeModule.forFeature([User])],
+  providers: [UsersResolver, UsersService, UsersRepository],
 })
 export class UsersModule {}
