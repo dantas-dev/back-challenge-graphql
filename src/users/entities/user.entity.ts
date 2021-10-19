d4466d5218e7cab0d3af95a3b58ca6fc0c5402be
uigor <uigor.silva@acad.ifma.edu.br>
2021-10-19 16:25:42 -0300

    feat: implementa campos da entidade User

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,12 @@
-export class User {}
+import { Field, ObjectType } from '@nestjs/graphql';
+@ObjectType()
+export class User {
+  @Field()
+  id: number;
+
+  @Field()
+  name: string;
+
+  @Field()
+  email: string;
+}
