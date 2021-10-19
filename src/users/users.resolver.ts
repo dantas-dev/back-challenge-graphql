c64443e32ff3731fee18d779cd08afb5e7c1f407
uigor <uigor.silva@acad.ifma.edu.br>
2021-10-19 17:50:43 -0300

    feat: importa usersService no usersResolver

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,9 +1,11 @@
 import { Resolver, Mutation } from '@nestjs/graphql';
 import { CreateUserInput } from './dto/create-user.input';
 import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
 
 @Resolver('User')
 export class UsersResolver {
+  constructor(private readonly usersService: UsersService) {}
   @Mutation('createUser')
   create(createUserInput: CreateUserInput): User {
     return new User();
