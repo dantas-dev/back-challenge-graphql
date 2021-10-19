b5be82fccdfb6d8ad3f98829282a2cad0b4a47ef
uigor <uigor.silva@acad.ifma.edu.br>
2021-10-19 18:14:11 -0300

    feat: implementa conexao mysql com sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { User } from './users/entities/user.entity';
+import { GraphQLModule } from '@nestjs/graphql/dist/graphql.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersModule } from './users/users.module';
-
+require('dotenv/config');
 @Module({
-  imports: [UsersModule],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      models: [User],
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
