bcaa3910a7146ca14a2909805f84c5d303b0088a
uigor <uigor.silva@acad.ifma.edu.br>
2021-10-19 16:43:05 -0300

    feat: cria metodo create em users.resolver

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,9 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 
 @Resolver('User')
-export class UsersResolver {}
+export class UsersResolver {
+  @Mutation('createUser')
+  create() {
+    return 'Retorna um novo usuario salvo no banco de dados';
+  }
+}
