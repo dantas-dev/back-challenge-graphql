e0e261f5a141a8d0dc0047c667dc3e6edfb0f7f5
uigor <uigor.silva@acad.ifma.edu.br>
2021-10-19 18:15:22 -0300

    feat: cria metodo create

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserInput } from './dto/create-user.input';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  create(createUserInput: CreateUserInput) {
+    throw new Error('Method not implemented.');
+  }
+}
