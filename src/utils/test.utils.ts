077ea4aaadc4d54fa2fcb2344a38f3535ec637f0
uigor <uigor.silva@acad.ifma.edu.br>
2021-10-19 17:38:14 -0300

    test: adiciciona getAValidCreateUserInput

--- src/utils/test.utils.ts
+++ src/utils/test.utils.ts
@@ -1,9 +1,17 @@
+import { CreateUserInput } from '../users/dto/create-user.input';
 import { User } from '../users/entities/user.entity';
 
 export class TestUtils {
   static getAValidUser(): User {
     const user = new User();
     user.id = 1;
+    user.name = 'Uigor Marshall';
+    user.email = 'uigor@marshall.com';
+    return user;
+  }
+
+  static getAValidCreateUserInput(): CreateUserInput {
+    const user = new CreateUserInput();
     user.name = 'Uigor Marshall';
     user.email = 'uigor@marshall.com';
     return user;
