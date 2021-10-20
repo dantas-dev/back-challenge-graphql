73fc81639278e955ef103cd37e56320c16c59bde
uigor <uigor.silva@acad.ifma.edu.br>
2021-10-19 21:04:46 -0300

    feat: implementa userRepository

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserInput } from './dto/create-user.input';
+import { User } from './entities/user.entity';
+import { UsersRepository } from './repositories/users.repository';
 
 @Injectable()
 export class UsersService {
-  create(createUserInput: CreateUserInput) {
-    throw new Error('Method not implemented.');
+  constructor(private readonly usersRepository: UsersRepository) {}
+  create(createUserInput: CreateUserInput): Promise<User> {
+    return this.usersRepository.create(createUserInput);
   }
 }
