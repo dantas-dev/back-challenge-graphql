24fb8a1b0d0ea887c6731f42ce07e03e97d3c3c2
uigor <uigor.silva@acad.ifma.edu.br>
2021-10-20 18:09:54 -0300

    test: adiciona 'FindAll: Deve retornar uma lista de Projects válidos'

--- src/projects/projects.service.spec.ts
+++ src/projects/projects.service.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TestUtils } from '../utils/test.utils';
 import { ProjectsService } from './projects.service';
+import { ProjectsRepository } from './repositories/projects.repository';
 
 describe('ProjectsService', () => {
   let service: ProjectsService;
-
+  const mockUsersRepository = {
+    create: jest.fn(),
+    findAll: jest.fn(),
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProjectsService],
+      providers: [
+        ProjectsService,
+        {
+          provide: ProjectsRepository,
+          useValue: mockUsersRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<ProjectsService>(ProjectsService);
@@ -19,6 +30,14 @@ describe('ProjectsService', () => {
   describe('FindAll', () => {
     it('Deve ser definido', () => {
       expect(service.findAll).toBeDefined();
+    });
+
+    it('Deve retornar uma lista de Projects válidos', async () => {
+      const projects = await TestUtils.getAValidListProjects();
+
+      mockUsersRepository.findAll.mockReturnValue(projects);
+      const result = await service.findAll();
+      expect(result).toEqual(projects);
     });
   });
 });
