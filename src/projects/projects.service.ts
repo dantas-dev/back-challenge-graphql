3b08df75938351c7830483bddd979e23abef99f1
uigor <uigor.silva@acad.ifma.edu.br>
2021-10-20 17:50:51 -0300

    feat: importa repositorio no service

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Project } from './entities/project.entity';
+import { ProjectsRepository } from './repositories/projects.repository';
 
 @Injectable()
-export class ProjectsService {}
+export class ProjectsService {
+  constructor(private readonly projectsRepository: ProjectsRepository) {}
+  findAll(): Project[] {
+    const fakeList: Project[] = [];
+    return fakeList;
+  }
+}
