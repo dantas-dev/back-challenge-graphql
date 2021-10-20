0432db867a9bb4c40de538bcaa7220bd92816512
uigor <uigor.silva@acad.ifma.edu.br>
2021-10-20 17:08:14 -0300

    feat: adiciona model de Project

---
+++ src/projects/entities/project.entity.ts
@@ -0,0 +1,32 @@
+import {
+  BelongsTo,
+  Column,
+  ForeignKey,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import { Field, ObjectType } from '@nestjs/graphql';
+import { User } from '../../users/entities/user.entity';
+@Table({
+  modelName: 'projects',
+})
+@ObjectType()
+export class Project extends Model {
+  @Field()
+  id: number;
+
+  @Column
+  @Field()
+  name: string;
+
+  @Column
+  @Field()
+  email: string;
+
+  @ForeignKey(() => User)
+  @Column
+  userId: number;
+
+  @BelongsTo(() => User)
+  user: User;
+}
