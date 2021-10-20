a09ff88e17da0f965a6daeafabc6c648b5b480fd
uigor <uigor.silva@acad.ifma.edu.br>
2021-10-20 18:11:15 -0300

    feat: adiciona import do repositorio no modulo

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -3,9 +3,10 @@ import { ProjectsService } from './projects.service';
 import { ProjectsResolver } from './projects.resolver';
 import { Project } from './entities/project.entity';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ProjectsRepository } from './repositories/projects.repository';
 
 @Module({
   imports: [SequelizeModule.forFeature([Project])],
-  providers: [ProjectsResolver, ProjectsService],
+  providers: [ProjectsResolver, ProjectsService, ProjectsRepository],
 })
 export class ProjectsModule {}
