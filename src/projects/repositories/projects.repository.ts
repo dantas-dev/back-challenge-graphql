2e115384012af591c33364ca7706cb7700efdc06
uigor <uigor.silva@acad.ifma.edu.br>
2021-10-20 17:39:35 -0300

    feat: implementa repositorio de Project

---
+++ src/projects/repositories/projects.repository.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { CreateProjectInput } from '../dto/create-project.input';
+import { Project } from '../entities/project.entity';
+@Injectable()
+export class UsersRepository {
+  constructor(
+    @InjectModel(Project)
+    private projectModel: typeof Project,
+  ) {}
+  async create(createProjectInput: CreateProjectInput): Promise<Project> {
+    return this.projectModel.create(createProjectInput);
+  }
+
+  findAll(): Promise<Project[]> {
+    return this.projectModel.findAll();
+  }
+}
