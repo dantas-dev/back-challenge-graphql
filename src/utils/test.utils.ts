cc99ddd6026b3b912eaeb16d7c4e93ee126364ba
uigor <uigor.silva@acad.ifma.edu.br>
2021-10-19 21:03:29 -0300

    fix: corrige criacao de user valido

--- src/utils/test.utils.ts
+++ src/utils/test.utils.ts
@@ -2,12 +2,12 @@ import { CreateUserInput } from '../users/dto/create-user.input';
 import { User } from '../users/entities/user.entity';
 
 export class TestUtils {
-  static getAValidUser(): User {
-    const user = new User();
-    user.id = 1;
-    user.name = 'Uigor Marshall';
-    user.email = 'uigor@marshall.com';
-    return user;
+  static async getAValidUser(): Promise<User> {
+    return await User.create({
+      id: 1,
+      name: 'Uigor Marshall',
+      email: 'uigor@marshall.com',
+    });
   }
 
   static getAValidCreateUserInput(): CreateUserInput {
