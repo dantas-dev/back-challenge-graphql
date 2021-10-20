e04a98666b3f6f2797f2022ab0220f5a4a17e477
uigor <uigor.silva@acad.ifma.edu.br>
2021-10-20 17:29:49 -0300

    feat: implementa query projects

--- src/projects/projects.resolver.ts
+++ src/projects/projects.resolver.ts
@@ -1,7 +1,13 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
+import { Project } from './entities/project.entity';
 import { ProjectsService } from './projects.service';
 
 @Resolver('Project')
 export class ProjectsResolver {
   constructor(private readonly projectsService: ProjectsService) {}
+
+  @Query(() => [Project], { name: 'projects' })
+  findAll(): Project[] {
+    return [];
+  }
 }
