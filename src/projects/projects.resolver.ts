746dfd9dca3037f0b2fe196e41b68182353eb233
uigor <uigor.silva@acad.ifma.edu.br>
2021-10-20 17:39:08 -0300

    feat: implementa metodo create

--- src/projects/projects.resolver.ts
+++ src/projects/projects.resolver.ts
@@ -1,11 +1,17 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateProjectInput } from './dto/create-project.input';
 import { Project } from './entities/project.entity';
 import { ProjectsService } from './projects.service';
 
 @Resolver('Project')
 export class ProjectsResolver {
   constructor(private readonly projectsService: ProjectsService) {}
-
+  @Mutation(() => Project, {
+    name: 'createUser',
+  })
+  create(@Args('createUserInput') createUserInput: CreateProjectInput) {
+    return null;
+  }
   @Query(() => [Project], { name: 'projects' })
   findAll(): Project[] {
     return [];
