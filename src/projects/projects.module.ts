67d51cb323f27281d6c65e3ef92cb75a51cbcf5d
uigor <uigor.silva@acad.ifma.edu.br>
2021-10-20 17:11:34 -0300

    feat: adiciona conexao sequelize para feat de Project

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { ProjectsResolver } from './projects.resolver';
+import { Project } from './entities/project.entity';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  providers: [ProjectsResolver, ProjectsService]
+  imports: [SequelizeModule.forFeature([Project])],
+  providers: [ProjectsResolver, ProjectsService],
 })
 export class ProjectsModule {}
