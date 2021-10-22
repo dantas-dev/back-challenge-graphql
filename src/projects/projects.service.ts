2f90fc20f8d96f2b73efbb1da88fd01320358050
uigormarshall <uigor.silva@acad.ifma.edu.br>
2021-10-22 02:41:29 -0300

    feat: cria metodo create no service

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { CreateProjectInput } from './dto/create-project.input';
 import { Project } from './entities/project.entity';
 import { ProjectsRepository } from './repositories/projects.repository';
 
@@ -7,5 +8,9 @@ export class ProjectsService {
   constructor(private readonly projectsRepository: ProjectsRepository) {}
   findAll(): Promise<Project[]> {
     return this.projectsRepository.findAll();
+  }
+
+  create(createProjectInput: CreateProjectInput) {
+    return this.projectsRepository.create(createProjectInput);
   }
 }
