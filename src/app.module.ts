37bcc45fcb69cb27682078e16946c13517a23385
thiagotesla <thiagobarros95@gmail.com>
2021-11-30 16:44:05 -0600

    feature: adiciona arquivos básicos do module projects

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ProjectsModule],
   controllers: [AppController],
   providers: [AppService],
 })
