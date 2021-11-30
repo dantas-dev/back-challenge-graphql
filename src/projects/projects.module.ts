9043a4c9c6b74a21953a2c4a9cf4505f21c6293c
thiagotesla <thiagobarros95@gmail.com>
2021-11-30 17:44:05 -0600

    feature: adiciona SequelizeModule ao imports do projects.module, adiciona ProjectsResolver, ProjectsService e ProjectsRepository ao providers e adiciona SequelizeModule ao exports

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ProjectsService } from './projects.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Project } from './entities/projects.entity';
 import { ProjectsResolver } from './projects.resolver';
+import { ProjectsService } from './projects.service';
+import { ProjectsRepository } from './repositories/projects.repository';
 
 @Module({
-  providers: [ProjectsResolver, ProjectsService]
+  imports: [SequelizeModule.forFeature([Project])],
+  exports: [SequelizeModule],
+  providers: [ProjectsResolver, ProjectsService, ProjectsRepository]
 })
 export class ProjectsModule {}
