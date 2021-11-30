ed5b9d7d956669d76d7f4c19f2dc7a6cb73cee59
thiagotesla <thiagobarros95@gmail.com>
2021-11-30 17:51:06 -0600

    feature: cria projects.repository

---
+++ src/projects/repositories/projects.repository.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from 'src/users/entities/users.entity';
+import { CreateProjectInput } from '../dto/create-project.input';
+import { Project } from '../entities/projects.entity';
+@Injectable()
+export class ProjectsRepository {
+  constructor(
+    @InjectModel(Project)
+    private projectModel: typeof Project,
+  ) {}
+  async create(createProjectInput: CreateProjectInput): Promise<Project> {
+    const project = await this.projectModel.create(createProjectInput);
+    return this.findOne(project.id);
+  }
+
+  async findOne(id: number): Promise<Project> {
+    return await this.projectModel.findByPk(id, {
+      include: {
+        model: User,
+        required: true,
+      },
+    });
+  }
+
+  findAll(): Promise<Project[]> {
+    return this.projectModel.findAll({
+      include: {
+        model: User,
+        required: true,
+      },
+    });
+  }
+}
