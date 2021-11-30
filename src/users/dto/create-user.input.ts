78b93ad423ae88c3635bbd486b961243de339232
thiagotesla <thiagobarros95@gmail.com>
2021-11-30 17:54:33 -0600

    feature: reescreve create-user-input para adicionar os campos da entidade

--- src/users/dto/create-user.input.ts
+++ src/users/dto/create-user.input.ts
@@ -1,7 +1,10 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
+import { InputType, Field } from '@nestjs/graphql';
 
 @InputType()
 export class CreateUserInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field()
+  name: string;
+
+  @Field()
+  email: string;
 }
