0cdbfa8145ea6a40ebda004435c7e0c375e390d4
thiagotesla <thiagobarros95@gmail.com>
2021-11-30 18:08:12 -0600

    feature: adiciona dependencias e cria arquivos para criação de migrations

---
+++ src/database/config.ts
@@ -0,0 +1,27 @@
+require('dotenv/config');
+module.exports = {
+  development: {
+    dialect: 'mysql',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  },
+  test: {
+    dialect: 'mysql',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  },
+  production: {
+    dialect: 'mysql',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  },
+};
