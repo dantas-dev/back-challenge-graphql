bd86b5f1b889c8ee604818a9c00cbd8564fa072c
Rafael Figueiredo <rafaael.figueiredo@gmail.com>
2021-12-15 20:39:06 -0300

    refactor(user.module): remove unnecessary imports

--- src/modules/users/dtos/userClass.dto.ts
+++ src/modules/users/dtos/userClass.dto.ts
@@ -1,12 +1,12 @@
-import { FilterableField, IDField } from '@nestjs-query/query-graphql';
-import { Field, GraphQLISODateTime, ID, ObjectType } from '@nestjs/graphql';
+import { FilterableField } from '@nestjs-query/query-graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
 import { BaseDTO } from '../../../common/baseDto/base.dto';
 
 @ObjectType('User')
 export class UserClassDTO extends BaseDTO {
   @FilterableField()
-  name: String;
+  name: string;
 
   @Field()
-  email: String;
+  email: string;
 }
