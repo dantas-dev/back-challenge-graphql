42cc88b67718a295a626a5adeca77e1bf2bd0f0a
Rafael Figueiredo <rafaael.figueiredo@gmail.com>
2021-12-16 22:52:23 -0300

    build(deps): add helmet to help us secure more our app

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
